74bbd765c32d966f9c724f71d0bcf7038e293526
Mark Rickerby <me@maetl.net>
2018-12-01 13:36:55 +1300

    Fix off by one error in template context vars

--- lib/story_entities.rb
+++ lib/story_entities.rb
@@ -32,67 +32,67 @@ class StoryEntities
     context[:language] = ["English", "Maori", "English"]
 
     self.max_expansions("norp").times do |i|
-      context["norp#{i}".to_sym] = "{$norp}"
+      context["norp#{i+1}".to_sym] = "{$norp}"
     end
 
     self.max_expansions("person").times do |i|
-      context["person#{i}".to_sym] = "{$person}"
+      context["person#{i+1}".to_sym] = "{$person}"
     end
 
     self.max_expansions("work_of_art").times do |i|
-      context["work_of_art#{i}".to_sym] = "{$work_of_art}"
+      context["work_of_art#{i+1}".to_sym] = "{$work_of_art}"
     end
 
     self.max_expansions("org").times do |i|
-      context["org#{i}".to_sym] = "{$org}"
+      context["org#{i+1}".to_sym] = "{$org}"
     end
 
     self.max_expansions("gpe").times do |i|
-      context["gpe#{i}".to_sym] = "{$gpe}"
+      context["gpe#{i+1}".to_sym] = "{$gpe}"
     end
 
     self.max_expansions("quantity").times do |i|
-      context["quantity#{i}".to_sym] = "{$quantity}"
+      context["quantity#{i+1}".to_sym] = "{$quantity}"
     end
 
     self.max_expansions("event").times do |i|
-      context["event#{i}".to_sym] = "{$event}"
+      context["event#{i+1}".to_sym] = "{$event}"
     end
 
     self.max_expansions("time").times do |i|
-      context["time#{i}".to_sym] = "{$time}"
+      context["time#{i+1}".to_sym] = "{$time}"
     end
 
     self.max_expansions("law").times do |i|
-      context["law#{i}".to_sym] = "{$law}"
+      context["law#{i+1}".to_sym] = "{$law}"
     end
 
     self.max_expansions("product").times do |i|
-      context["product#{i}".to_sym] = "{$product}"
+      context["product#{i+1}".to_sym] = "{$product}"
     end
 
     self.max_expansions("percent").times do |i|
-      context["percent#{i}".to_sym] = "{$percent}"
+      context["percent#{i+1}".to_sym] = "{$percent}"
     end
 
     self.max_expansions("loc").times do |i|
-      context["loc#{i}".to_sym] = "{$loc}"
+      context["loc#{i+1}".to_sym] = "{$loc}"
     end
 
     self.max_expansions("fac").times do |i|
-      context["fac#{i}".to_sym] = "{$fac}"
+      context["fac#{i+1}".to_sym] = "{$fac}"
     end
 
     self.max_expansions("date").times do |i|
-      context["date#{i}".to_sym] = "{$date}"
+      context["date#{i+1}".to_sym] = "{$date}"
     end
 
     self.max_expansions("money").times do |i|
-      context["money#{i}".to_sym] = "{$money}"
+      context["money#{i+1}".to_sym] = "{$money}"
     end
 
     self.max_expansions("language").times do |i|
-      context["language#{i}".to_sym] = "{$language}"
+      context["language#{i+1}".to_sym] = "{$language}"
     end
 
     context
